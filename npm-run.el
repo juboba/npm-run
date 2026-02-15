2997303e5565e56599aabd74bc090ca136cde215
Julio Borja Barra <juboba@cantina.ai>
2026-02-15 11:39:12 +0100

    Add license

--- npm-run.el
+++ npm-run.el
@@ -8,6 +8,21 @@
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience, tools, npm, scripts, project
 
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; npm-run provides an interactive command to run npm scripts from any
 ;; package.json file in your project.
